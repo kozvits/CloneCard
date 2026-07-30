a95f12577fee25135244768fe4c5c6cf55af2937
kozvits <kozvits@gmail.com>
2026-07-30 15:23:01 +0300

    Fix Compose compiler: use composeOptions for Kotlin 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
 }
 
@@ -39,6 +38,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
 
     packaging {
